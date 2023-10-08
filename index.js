eae385e5ef08f43995f2bed4fb7e498deb6d174a
shekinah <nyamburamary1212@gmail.com>
2023-10-08 14:35:43 +0300

    idex.html and bootstrap

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Sample movie data (replace with actual data from your API)
-    const movieData = {
-        title: 'The Giant Gila Monster',
-        runtime: '108',
-        capacity: 30,
-        showtime: '04:00 PM',
-        tickets_sold: 27,
-        description: 'A giant lizard terrorizes a rural Texas community...',
-        poster: 'https://www.gstatic.com/tv/thumb/v22vodart/2157/p2157_v_v8_ab.jpg',
-    };
-
     const filmsList = document.getElementById('films');
     const movieDetails = document.getElementById('movie-details');
     const buyTicketButton = document.getElementById('buy-ticket');
 
-    // Display movie details
+    // Function to fetch movie data from the server
+    function fetchMovies() {
+        fetch('http://localhost:3000/films') // Replace with your server URL
+            .then(response => response.json())
+            .then(movies => {
+                // Populate the movie list
+                movies.forEach(movie => {
+                    const movieItem = document.createElement('li');
+                    movieItem.textContent = movie.title;
+                    movieItem.classList.add('film', 'item');
+                    movieItem.setAttribute('data-movie-id', movie.id);
+                    filmsList.appendChild(movieItem);
+
+                    // Add click event to movie items
+                    movieItem.addEventListener('click', () => {
+                        displayMovieDetails(movie);
+                    });
+                });
+
+                // Display details of the first movie initially
+                if (movies.length > 0) {
+                    displayMovieDetails(movies[0]);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching movies:', error);
+            });
+    }
+
+    // Function to display movie details
     function displayMovieDetails(movie) {
         movieDetails.innerHTML = `
             <img src="${movie.poster}" alt="${movie.title}" class="img-fluid">
@@ -24,20 +42,35 @@ document.addEventListener('DOMContentLoaded', () => {
             <p>Showtime: ${movie.showtime}</p>
             <p id="ticket-num">Tickets Available: ${movie.capacity - movie.tickets_sold}</p>
         `;
+
+        // Update Buy Ticket button click event
+        buyTicketButton.disabled = movie.capacity - movie.tickets_sold <= 0;
+        buyTicketButton.textContent = buyTicketButton.disabled ? 'Sold Out' : 'Buy Ticket';
+
+        // Add click event to Buy Ticket button
+        buyTicketButton.addEventListener('click', () => {
+            buyTicket(movie);
+        });
     }
 
-    // Initial display of movie details
-    displayMovieDetails(movieData);
-
-    // Buy Ticket button click event
-    buyTicketButton.addEventListener('click', () => {
-        const remainingTickets = movieData.capacity - movieData.tickets_sold;
-        if (remainingTickets > 0) {
-            movieData.tickets_sold++;
-            document.getElementById('ticket-num').textContent = `Tickets Available: ${remainingTickets - 1}`;
-        } else {
-            buyTicketButton.textContent = 'Sold Out';
-            buyTicketButton.disabled = true;
+    // Function to buy a ticket
+    function buyTicket(movie) {
+        if (movie.capacity - movie.tickets_sold <= 0) {
+            return; // Movie is sold out, do not proceed
         }
-    });
+
+        // Simulate buying a ticket (update tickets_sold)
+        movie.tickets_sold++;
+        const remainingTickets = movie.capacity - movie.tickets_sold;
+
+        // Update the displayed ticket count
+        document.getElementById('ticket-num').textContent = `Tickets Available: ${remainingTickets}`;
+
+        // Update Buy Ticket button state
+        buyTicketButton.disabled = remainingTickets <= 0;
+        buyTicketButton.textContent = buyTicketButton.disabled ? 'Sold Out' : 'Buy Ticket';
+    }
+
+    // Fetch movies and initialize the page
+    fetchMovies();
 });
