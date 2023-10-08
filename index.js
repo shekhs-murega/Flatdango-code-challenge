fb41b61e920bf4a695daf6135ae2630be6ec2da2
shekinah <nyamburamary1212@gmail.com>
2023-10-08 23:37:52 +0300

    complete Flatdango challenge

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const filmsList = document.getElementById('films');
-    const movieDetails = document.getElementById('movie-details');
-    const buyTicketButton = document.getElementById('buy-ticket');
-
-    // Function to fetch movie data from the server
+    const filmsList = document.getElementById('dividedlist');
+    const movieDetails = document.getElementById('buymovie');
+// Function to fetch movie data from the server
     function fetchMovies() {
-        fetch('http://localhost:3000/films') // Replace with your server URL
+        fetch('http://localhost:3000/films')
             .then(response => response.json())
             .then(movies => {
-                // Populate the movie list
                 movies.forEach(movie => {
                     const movieItem = document.createElement('li');
                     movieItem.textContent = movie.title;
@@ -16,13 +13,12 @@ document.addEventListener('DOMContentLoaded', () => {
                     movieItem.setAttribute('data-movie-id', movie.id);
                     filmsList.appendChild(movieItem);
 
-                    // Add click event to movie items
-                    movieItem.addEventListener('click', () => {
-                        displayMovieDetails(movie);
-                    });
+                movieItem.addEventListener('click', (e) => {
+                    e.preventDefault()
+                    displayMovieDetails(movie);
                 });
-
-                // Display details of the first movie initially
+            });
+// Display details of the first movie initially
                 if (movies.length > 0) {
                     displayMovieDetails(movies[0]);
                 }
@@ -32,45 +28,52 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     }
 
-    // Function to display movie details
+// Function to display movie details
     function displayMovieDetails(movie) {
         movieDetails.innerHTML = `
-            <img src="${movie.poster}" alt="${movie.title}" class="img-fluid">
-            <h2 class="title">${movie.title}</h2>
+        <img src="${movie.poster}" alt="${movie.title}" class="img-fluid">
+            <h2 id="title">Title: ${movie.title}</h2>
+            <p id="description">Description: ${movie.description}</p>
             <p id="runtime">Runtime: ${movie.runtime} minutes</p>
-            <p class="content">${movie.description}</p>
-            <p>Showtime: ${movie.showtime}</p>
-            <p id="ticket-num">Tickets Available: ${movie.capacity - movie.tickets_sold}</p>
-        `;
-
-        // Update Buy Ticket button click event
-        buyTicketButton.disabled = movie.capacity - movie.tickets_sold <= 0;
-        buyTicketButton.textContent = buyTicketButton.disabled ? 'Sold Out' : 'Buy Ticket';
-
-        // Add click event to Buy Ticket button
-        buyTicketButton.addEventListener('click', () => {
-            buyTicket(movie);
+            <p id="showtime">Showtime: ${movie.showtime}</p>
+            <p id="tickets-available">Tickets Available: ${movie.capacity - movie.tickets_sold}</p>;
+`
+// Create a "Buy Ticket" button
+        const buyTicketButton = document.createElement('button');
+        buyTicketButton.id = 'buyticket';
+        buyTicketButton.textContent = 'Buy Ticket';
+        movieDetails.appendChild(buyTicketButton);
+// Calculate available tickets by subtracting tickets_sold from capacity
+        let availableTickets = movie.capacity - movie.tickets_sold;
+        const ticketsAvailable = document.getElementById('tickets-available');
+        ticketsAvailable.textContent = "Tickets Available: " + availableTickets;
+// Add a click event listener to the "Buy Ticket" button
+        buyTicketButton.addEventListener("click", (e) => {
+            e.preventDefault();
+// Check if there are available tickets
+            if (availableTickets > 0) {
+// Decrement tickets_sold count
+                movie.tickets_sold++;
+// Update available tickets
+                availableTickets--;
+// Update the frontend to reflect the changes
+                ticketsAvailable.textContent = "Tickets Available: " + availableTickets;
+// Check if all tickets are sold out
+                if (availableTickets === 0) {
+// Disable the "Buy Ticket" button
+                    buyTicketButton.disabled = true;
+                    buyTicketButton.textContent = "Sold Out";
+                }
+// Show a success alert message
+        window.alert("You have successfully purchased a ticket!");
+            } else {
+// No available tickets
+                buyTicketButton.disabled = true;
+                buyTicketButton.textContent = "Sold Out";
+            }
         });
     }
 
-    // Function to buy a ticket
-    function buyTicket(movie) {
-        if (movie.capacity - movie.tickets_sold <= 0) {
-         return window.alert("You have successfully purchased a ticket!");
-            }
-
-        // Simulate buying a ticket (update tickets_sold)
-        movie.tickets_sold++;
-        const remainingTickets = movie.capacity - movie.tickets_sold;
-
-        // Update the displayed ticket count
-        document.getElementById('ticket-num').textContent = `Tickets Available: ${remainingTickets}`;
-
-        // Update Buy Ticket button state
-        buyTicketButton.disabled = remainingTickets <= 0;
-        buyTicketButton.textContent = buyTicketButton.disabled ? 'Sold Out' : 'Buy Ticket';
-    }
-
-    // Fetch movies and initialize the page
+// Fetch movies and initialize the page
     fetchMovies();
 });
